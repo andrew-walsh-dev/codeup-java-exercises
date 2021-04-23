15d09a5251831e48c2c031464afed283b4b8604f
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-04-23 11:09:48 -0500

    finished java syntax, types, variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,42 @@
 public class HelloWorld {
-    public static void main(String[] args){
-        System.out.print("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        {
+            int myFavoriteNumber = 5;
+            String myString = "I am a string.";
+            float myNumber = 3.14F;
+            System.out.println(myNumber);
+        }
+        {
+            int x = 5;
+            System.out.println(x++);
+            System.out.println(x);
+        }
+        {
+            int x = 5;
+            System.out.println(++x);
+            System.out.println(x);
+        }
+        {
+            int x = 4;
+            x += 5;
+        }
+        {
+            int x = 3;
+            int y = 4;
+            y *= x;
+        }
+        {
+            int x = 10;
+            int y = 2;
+            x /= y;
+            y -= x;
+        }
+        {
+            //overflow
+            int x = Integer.MAX_VALUE + 1;
+            System.out.println(x);
+        }
     }
 }
+
