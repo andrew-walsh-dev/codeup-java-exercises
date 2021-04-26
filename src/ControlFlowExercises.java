034f9618efd0f1d1ed9962b8043a2508c190f55c
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-04-26 14:29:23 -0500

    finished exercise #4

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -44,22 +44,54 @@ public class ControlFlowExercises {
         //         }
         //     }
         // }
-        //3
+        // //3
+        // {
+        //     boolean more;
+        //     do{
+        //         System.out.println("What number would you like to go up to? ");
+        //         int upTo = in.nextInt();
+        //         System.out.println("\nHere is your table: \n");
+        //         System.out.printf("%7s | %7s | %5s%n", "number", "squared", "cubed");
+        //         System.out.printf("%7s | %7s | %5s%n", "------", "-------", "-----");
+        //         for (int i = 1; i <= upTo; i++){
+        //             System.out.printf("%7d | %7d | %5d%n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
+        //         }
+        //         System.out.println("\nWould you like to continue? (Y/N) ");
+        //         String choice = in.next();
+        //         more = choice.equalsIgnoreCase("Y");
+        //     } while(more);
+        // }
+        //4
         {
             boolean more;
+            int grade;
+            String choice;
             do{
-                System.out.println("What number would you like to go up to? ");
-                int upTo = in.nextInt();
-                System.out.println("\nHere is your table: \n");
-                System.out.printf("%7s | %7s | %5s%n", "number", "squared", "cubed");
-                System.out.printf("%7s | %7s | %5s%n", "------", "-------", "-----");
-                for (int i = 1; i <= upTo; i++){
-                    System.out.printf("%7d | %7d | %5d%n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
+                System.out.println("Enter a numerical grade from 0 to 100: ");
+                grade = in.nextInt();
+                while (grade > 100 || grade < 0){
+                    System.out.println("Please enter a valid grade (0-100): ");
+                    grade = in.nextInt();
                 }
-                System.out.println("\nWould you like to continue? (Y/N) ");
-                String choice = in.next();
+                if (grade >= 88){
+                    System.out.println("A");
+                }
+                else if (grade >= 80){
+                    System.out.println("B");
+                }
+                else if (grade >= 67){
+                    System.out.println("C");
+                }
+                else if (grade >= 60){
+                    System.out.println("D");
+                }
+                else{
+                    System.out.println("F");
+                }
+                System.out.println("Would you like to continue? (Y/N)");
+                choice = in.next();
                 more = choice.equalsIgnoreCase("Y");
-            } while(more);
+            } while (more);
         }
      }
 }
