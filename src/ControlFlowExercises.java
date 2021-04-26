1f660012ea9c31666322fbb9360d425bab368071
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-04-26 14:15:31 -0500

    finished exercise #2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,48 +2,48 @@ import java.util.Scanner;
 public class ControlFlowExercises {
      public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        //1a
-        {
-            int i = 5;
-            while (i <= 15){
-                System.out.println(i++);
-            }
-        }
-        //1b
-        {
-            int i = 2;
-            do {
-                System.out.println(i);
-                i = (int) Math.pow(i, 2);
-            } while(i <= 1000000);
-        }
-        //1c
-        {
-            for (int i = 5; i <= 15; i++){
-                System.out.println(i);
-            }
-
-            for (int i = 2; i < 1000000; i = (int) Math.pow(i, 2)){
-                System.out.println(i);
-            }
-        }
-        // //2
+        // //1a
         // {
-        //     for (int i = 1; i <= 100; i++){
-        //         if (i % 5 == 0 && i % 3 == 0){
-        //             System.out.println("FizzBuzz");
-        //         }
-        //         else if (i % 3 == 0){
-        //             System.out.println("Fizz");
-        //         }
-        //         else if (i % 5 == 0){
-        //             System.out.println("Buzz");
-        //         }
-        //         else{
-        //             System.out.println(i);
-        //         }
+        //     int i = 5;
+        //     while (i <= 15){
+        //         System.out.println(i++);
         //     }
         // }
+        // //1b
+        // {
+        //     int i = 2;
+        //     do {
+        //         System.out.println(i);
+        //         i = (int) Math.pow(i, 2);
+        //     } while(i <= 1000000);
+        // }
+        // //1c
+        // {
+        //     for (int i = 5; i <= 15; i++){
+        //         System.out.println(i);
+        //     }
+
+        //     for (int i = 2; i < 1000000; i = (int) Math.pow(i, 2)){
+        //         System.out.println(i);
+        //     }
+        // }
+        //2
+        {
+            for (int i = 1; i <= 100; i++){
+                if (i % 5 == 0 && i % 3 == 0){
+                    System.out.println("FizzBuzz");
+                }
+                else if (i % 3 == 0){
+                    System.out.println("Fizz");
+                }
+                else if (i % 5 == 0){
+                    System.out.println("Buzz");
+                }
+                else{
+                    System.out.println(i);
+                }
+            }
+        }
         // //3
         // {
         //     do{
