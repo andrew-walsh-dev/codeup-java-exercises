ca742482f474e7d1122ed42d2b79cb0492810854
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-05-06 11:51:14 -0500

    finished abstract class and interface exercises

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,16 +1,13 @@
 package shapes;
 
-public class Rectangle {
-    protected double length;
-    protected double width;
-    public Rectangle(double length, double width){
-        this.length = length;
-        this.width = width;
-    }
-    public double getArea(){
-        return this.length * this.width;
-    }
-    public double getPerimeter(){
-        return 2 * this.length + 2 * this.width;
-    }
+public class Rectangle extends Quadrilateral{
+  public Rectangle (double length, double width) {
+    super(length, width);
+  }
+  public void setLength(double length){
+    this.length = length;
+  }
+  public void setWidth(double width){
+    this.width = width;
+  }
 }
